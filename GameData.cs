74769e9fae751280961e414df89e688119537261
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2023-06-19 13:12:59 +0200

    changes

--- GameData.cs
+++ GameData.cs
@@ -9,14 +9,14 @@ namespace Pokemon
         public int[] Coordinates
         { get; set; } = new int[2] { 8, 8 };
         public string Name { get; set; } = "Player";
-        public List<Pokemon> Pokemons { get; set; } = new List<Pokemon> { new Bulbasaur(10, 0) };
+        public List<Pokemon> Pokemons { get; set; } = new List<Pokemon> { };
         public int ChosenPokemon { get; set; } = 0;
         public int Potions { get; set; } = 0;
         public int World { get; set; } = 0;
         public bool IsEncounter { get; set; } = false;
         public Pokemon CurrentEnemy;
         public bool CanInteract { get; set; } = false;
-        public bool IsCutscene { get; set; } = false;
+        public bool IsCutscene { get; set; } = true;
         public int CutsceneNumber { get; set; } = -1;
         public int CutsceneDialog { get; set; } = 0;
         public bool IsRunning { get; set; } = true;
